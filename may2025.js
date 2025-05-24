64c16998b382ae73daf3e2a5401a01208798250c
DeJuanP <ninerperry88@hotmail.com>
2025-05-23 21:46:21 -0400

    May 23rd

--- may2025.js
+++ may2025.js
@@ -32,3 +32,17 @@ function getDivisorsCnt(n){
   }
   return num;
 }
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+// Write a function "greet" that returns "hello world!"
+function greet (){
+  let sum = 1 + 1
+  if (sum == 2){
+     return "hello world!"
+  }
+ 
+} 
